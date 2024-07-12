dda98a49390dc1164bb5e947cd3d1995a7e4b221
Alphalikegithub <2908926129@qq.com>
2024-07-12 15:03:26 +0800

    leetcode 141.环形链表

---
+++ 141.环形链表.cpp
@@ -0,0 +1,49 @@
+#include <unordered_set>
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) 
+    : val(x),next(nullptr)
+    {
+
+    }
+};
+
+/*
+ * @lc app=leetcode.cn id=141 lang=cpp
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head || !head->next){
+            return false;
+        }
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+
+        while(slow != fast){
+            if(fast == nullptr || fast->next == nullptr){
+                return false;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return true;
+    }
+};
+// @lc code=end
+
