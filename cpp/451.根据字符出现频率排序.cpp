1630607dbbbccdbf57529d3eadc00443cda68687
Alphalikegithub <2908926129@qq.com>
2024-07-20 15:22:44 +0800

    leetcode451.根据字符出现频率排序

--- cpp/451.根据字符出现频率排序.cpp
+++ cpp/451.根据字符出现频率排序.cpp
@@ -1,4 +1,7 @@
 #include <string>
+#include <unordered_map>
+#include <algorithm>
+using std::unordered_map;
 using std::string;
 /*
  * @lc app=leetcode.cn id=451 lang=cpp
@@ -10,7 +13,20 @@ using std::string;
 class Solution {
 public:
     string frequencySort(string s) {
+        // 1. 使用哈希表统计每个字符的出现频率
+        unordered_map<char,int> dict; // 哈希表，键为字符，值为该字符的出现频率
+        for(auto c : s){
+            dict[c] ++;
+        }
+        // 2. 定义一个比较函数
+        auto compare = [&dict](const char& a,const char& b){
+            //dict[a] > dict[b]：按字符频率从高到低排序。
+            //dict[a] == dict[b] && (a < b)：如果频率相同，则按字符的字典序从小到大排序。
+            return (dict[a] > dict[b]  ) || (dict[a] == dict[b] && (a < b));
+        };
 
+        std::sort(s.begin(),s.end(),compare);
+        return s;
     }
 };
 // @lc code=end
