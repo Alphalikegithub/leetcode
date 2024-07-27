e7a8ae0b1bf81c85053a17842318fcc481494e9a
Alphalikegithub <2908926129@qq.com>
2024-07-27 08:34:30 +0800

    [744] 寻找比目标字母大的最小字母

---
+++ cpp/744.寻找比目标字母大的最小字母.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+using std::vector;
+/*
+ * @lc app=leetcode.cn id=744 lang=cpp
+ *
+ * [744] 寻找比目标字母大的最小字母
+ */
+
+// @lc code=start
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+
+    }
+};
+// @lc code=end
+
